5d2989854eefdf0cba7c6825fe565a88a87988fe
MLGPenguin <baww123@gmail.com>
2023-08-30 00:31:20 +1000

    Fix api-version & chat format

--- src/main/java/me/superpenguin/superparties/PartyChatListener.kt
+++ src/main/java/me/superpenguin/superparties/PartyChatListener.kt
@@ -13,7 +13,7 @@ class PartyChatListener(val parties: PartyPlugin): Listener {
         val sender = event.player
         if (parties.partyCommand.toggledPChat.contains(sender.uniqueId)) {
             event.isCancelled = true
-            sender.getTeam()?.messageAll("&8[&9Party Chat&8]&7${sender.name}&f: ${event.message}".toColour())
+            sender.getTeam()?.messageAll("&8[&9Party Chat&8]&7 ${sender.name}&f: ${event.message}".toColour())
                 ?: parties.partyCommand.toggledPChat.remove(sender.uniqueId).also {
                     sender.send("&cToggled party chat off as you are no longer in a party!")
                 }
